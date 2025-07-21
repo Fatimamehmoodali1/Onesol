68cc488dc2f56fceadcba68dfa65d92b319665b7
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-07-21 15:41:54 +0500

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,28 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      {/* Left: Logo */}
       <div className="nav-left">
-        <img
-          src="/images/logo.png"
-          alt="Logo"
-          width={50}
-          height={50}
-          className="nav-logo"
-        />
+        <img src="/images/logo.png" alt="Logo" width={50} height={50} className="nav-logo" />
       </div>
 
-      {/* Center Nav Links (Desktop) */}
+      {/* Hamburger */}
+      <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+
+      {/* Mobile Nav */}
+      <div className={`mobile-nav ${menuOpen ? 'show' : ''}`}>
+        <a href="home">Home</a>
+        <a href="about">About</a>
+        <a href="ourwork">Our Work</a>
+        <a href="services">Services</a>
+        <a href="blog">Blog</a>
+        <button className="expert-button">Speak to an Expert</button>
+      </div>
+
+      {/* Desktop Nav */}
       <div className="nav-center">
         <div className="nav-links-container">
           <a href="home">Home</a>
@@ -30,27 +40,7 @@ export default function Navbar() {
         </div>
       </div>
 
-      {/* Right: Button + Hamburger (hamburger shown only on mobile) */}
       <div className="nav-right">
-        <button className="expert-button">Speak to an Expert</button>
-
-        <div
-          className={`hamburger ${menuOpen ? 'open' : ''}`}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-      </div>
-
-      {/* Mobile Nav Slide-In */}
-      <div className={`mobile-nav ${menuOpen ? 'show' : ''}`}>
-        <a href="home" onClick={() => setMenuOpen(false)}>Home</a>
-        <a href="about" onClick={() => setMenuOpen(false)}>About</a>
-        <a href="ourwork" onClick={() => setMenuOpen(false)}>Our Work</a>
-        <a href="services" onClick={() => setMenuOpen(false)}>Services</a>
-        <a href="blog" onClick={() => setMenuOpen(false)}>Blog</a>
         <button className="expert-button">Speak to an Expert</button>
       </div>
     </nav>
