2064578dd65d0bad76a12e1ec4d669725a27f007
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-07-21 16:46:26 +0500

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,42 +8,41 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-  <div className="nav-left">
-    <img src="/images/logo.png" alt="Logo" width={50} height={50} className="nav-logo" />
-  </div>
+      <div className="nav-left">
+        <img src="/images/logo.png" alt="Logo" width={50} height={50} className="nav-logo" />
+      </div>
 
-  {/* Desktop Nav */}
-  <div className="nav-center">
-    <div className="nav-links-container">
-      <a href="home">Home</a>
-      <a href="about">About</a>
-      <a href="ourwork">Our Work</a>
-      <a href="services">Services</a>
-      <a href="blog">Blog</a>
-    </div>
-  </div>
+      {/* Hamburger */}
+      <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
 
-  <div className="nav-right">
-    <button className="expert-button">Speak to an Expert</button>
+      {/* Mobile Nav */}
+      <div className={`mobile-nav ${menuOpen ? 'show' : ''}`}>
+        <a href="home">Home</a>
+        <a href="about">About</a>
+        <a href="ourwork">Our Work</a>
+        <a href="services">Services</a>
+        <a href="blog">Blog</a>
+        <button className="expert-button">Speak to an Expert</button>
+      </div>
 
-    {/* Hamburger now placed here */}
-    <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
-      <span className="bar"></span>
-      <span className="bar"></span>
-      <span className="bar"></span>
-    </div>
-  </div>
+      {/* Desktop Nav */}
+      <div className="nav-center">
+        <div className="nav-links-container">
+          <a href="home">Home</a>
+          <a href="about">About</a>
+          <a href="ourwork">Our Work</a>
+          <a href="services">Services</a>
+          <a href="blog">Blog</a>
+        </div>
+      </div>
 
-  {/* Mobile Nav */}
-  <div className={`mobile-nav ${menuOpen ? 'show' : ''}`}>
-    <a href="home">Home</a>
-    <a href="about">About</a>
-    <a href="ourwork">Our Work</a>
-    <a href="services">Services</a>
-    <a href="blog">Blog</a>
-    <button className="expert-button">Speak to an Expert</button>
-  </div>
-</nav>
- 
+      <div className="nav-right">
+        <button className="expert-button">Speak to an Expert</button>
+      </div>
+    </nav>
   );
 }
