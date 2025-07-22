2452465be743ddfb65b50a8052b6f0f09fc3afa2
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-07-22 16:11:34 +0500

    page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function Blog() {
           <img src="/images/logo.png" alt="Logo" /> 
         </div> 
         <ul className="nav-links"> 
-          <li><a href="#">Home</a></li> 
+          <li><a href="/">Home</a></li> 
           <li><a href="about">About</a></li> 
           <li><a href="ourwork">Our Work</a></li> 
           <li><a href="services">Services</a></li> 
