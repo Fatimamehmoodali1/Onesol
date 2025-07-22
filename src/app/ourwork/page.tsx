5dd37b4ba888a12da0762e6087fed90510b95073
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-07-22 16:08:07 +0500

    page.tsx

--- src/app/ourwork/page.tsx
+++ src/app/ourwork/page.tsx
@@ -11,7 +11,7 @@ export default function OurWork () {
               <img src="/images/logo.png" alt="" />
              </div>
              <ul className="nav-links">
-               <li><a href="#">Home</a></li>
+               <li><a href="/">Home</a></li>
                <li><a href="about">About</a></li>
                <li><a href="ourwork">Our Work</a></li>
                <li><a href="services">Services</a></li>
